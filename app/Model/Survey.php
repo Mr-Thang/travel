ea03daf50c79c81db2e2a6511682156097d9e90a
Ho Thang <thanghuuna@gmail.com>
2018-10-13 16:41:14 +0700

    create model survey

--- app/Model/Survey.php
+++ app/Model/Survey.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Survey extends Model
 {
-    //
+	protected $fillable = [
+		'full_name',
+		'email',
+		'phone',
+		'address',
+		'service',
+		'fear_likes',
+		'question',
+	];
 }
