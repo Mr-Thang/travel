23f4e65583e3d19b0fa903544b896092612642fc
Ho Thang <thanghuuna@gmail.com>
2018-10-13 16:40:47 +0700

    create model info

--- app/Model/Info.php
+++ app/Model/Info.php
@@ -1,10 +1,31 @@
 <?php
 
+use Cviebrock\EloquentSluggable\Sluggable;
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Info extends Model
 {
-    //
+	use Sluggable;
+
+	protected $fillable = [
+		'title',
+		'content',
+		'admin_id',
+	];
+
+	public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+    
+	public function admin()
+	{
+		return $this->belongsTo('App\Model\Admin');
+	}
 }
