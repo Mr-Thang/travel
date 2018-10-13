aa4620f8fed643d1182b4453f1b480cb092b916b
Ho Thang <thanghuuna@gmail.com>
2018-10-13 16:40:33 +0700

    create model admin

--- app/Model/Admin.php
+++ app/Model/Admin.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Admin extends Model
 {
-    //
+	protected $fillable = [
+		'first_name',
+		'last_name',
+		'email',
+		'password',
+		'position'
+	];
+
+	public function infos()
+	{
+		return $this->hasMany('App\Model\Info');
+	}
 }
