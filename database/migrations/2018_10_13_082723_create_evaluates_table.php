a7bbb901aefb955b302e2ec5595e4c5b792fb2dc
Ho Thang <thanghuuna@gmail.com>
2018-10-13 15:30:12 +0700

    add migrate evaluates

---
+++ database/migrations/2018_10_13_082723_create_evaluates_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEvaluatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('evaluates', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('slug');
+            $table->string('email')->unique();
+            $table->text('content');
+            $table->integer('evaluateable_id')->unsigned()->index();
+            $table->string('evaluateable_type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('evaluates');
+    }
+}
