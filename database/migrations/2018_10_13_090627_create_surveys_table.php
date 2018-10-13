17c9ec08cc6fe581e44a982c7cfa4e95ef07718b
Ho Thang <thanghuuna@gmail.com>
2018-10-13 16:13:12 +0700

    create migrate survey

---
+++ database/migrations/2018_10_13_090627_create_surveys_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSurveysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('surveys', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('full_name');
+            $table->string('email')->unique();
+            $table->char('phone', 12);
+            $table->string('address');
+            $table->string('service');
+            $table->string('fear_likes');
+            $table->text('question');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('surveys');
+    }
+}
