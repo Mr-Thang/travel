e1a92c177a8f088528beba446c5549b187d3dd1a
Ho Thang <thanghuuna@gmail.com>
2018-10-13 12:35:53 +0700

    migrate tour

---
+++ database/migrations/2018_10_13_053136_create_tours_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateToursTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tours', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('slug');
+            $table->text('highlights');
+            $table->string('vehicle');
+            $table->string('schedule');
+            $table->string('departure_schedule');
+            $table->integer('admin_id')->unsigned()->index();
+            $table->tinyInteger('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tours');
+    }
+}
