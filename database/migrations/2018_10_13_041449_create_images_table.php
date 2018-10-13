bcd516500d50d8c66dc0ce199fbf9dde98d360d3
Ho Thang <thanghuuna@gmail.com>
2018-10-13 15:24:27 +0700

    edit migrate image

--- database/migrations/2018_10_13_041449_create_images_table.php
+++ database/migrations/2018_10_13_041449_create_images_table.php
@@ -17,7 +17,7 @@ class CreateImagesTable extends Migration
             $table->increments('id');
             $table->text('link_url');
             $table->string('imageable_type');
-            $table->integer('imageable_type')->unsigned()->index();
+            $table->integer('imageable_id')->unsigned()->index();
             $table->timestamps();
         });
     }
