aa475c57cdb7701c041827fc786d5dd4a32a3eb7
Ho Thang <thanghuuna@gmail.com>
2018-10-16 09:44:48 +0700

    add model comment

--- app/Model/Comment.php
+++ app/Model/Comment.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-	// 
+	protected $fillable = [
+		'comment',
+		'user_id',
+		'post_id',
+	];
+
+	public function user()
+	 {
+	 	return $this->belongsTo('App\Model\User');
+	 } 
+
+	 public function post()
+	 {
+	 	return $this->belongsTo('App\Model\Post');
+	 }
 }
